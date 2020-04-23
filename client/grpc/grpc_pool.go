91fdc8bd36b469e75f407614cf097e5ed6184986
杨铭哲 <yangmz@weipaitang.com>
2020-04-23 16:56:51 +0800

    conn reuse

--- client/grpc/grpc_pool.go
+++ client/grpc/grpc_pool.go
@@ -1,83 +1,211 @@
 package grpc
 
 import (
+	"fmt"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
 )
 
+var (
+	defaultDebugMode        = false           // debug模式
+	defaultKeepAliveTime    = time.Second * 5 // 保活间隔时间
+	defaultKeepAliveTimeout = time.Second * 2 //
+
+	// just for test
+	pickDebugMode = false // 调试复用情况
+	newConnNums   = 0     // 新建连接数
+)
+
+func pool_init() {
+	// keepalive time
+	if kp := os.Getenv("MICRO_CLIENT_POOL_KEEPALIVE_TIME"); kp != "" {
+		if keepalive, err := strconv.Atoi(kp); err == nil && keepalive > 0 {
+			defaultKeepAliveTime = time.Duration(keepalive) * time.Second
+		}
+	}
+	// keepalive timeout
+	if kpout := os.Getenv("MICRO_CLIENT_POOL_KEEPALIVE_TIMEOUT"); kpout != "" {
+		if keepaliveTimeOut, err := strconv.Atoi(kpout); err == nil && keepaliveTimeOut > 0 {
+			defaultKeepAliveTimeout = time.Duration(keepaliveTimeOut) * time.Second
+		}
+	}
+	// debug mode
+	if debug := os.Getenv("MICRO_CLIENT_POOL_DEBUG"); debug == "true" {
+		defaultDebugMode = true
+	}
+}
+
 type pool struct {
-	size int
-	ttl  int64
+	size  int
+	ttl   int64
+	debug bool
+
+	KeepAliveTime    time.Duration
+	KeepAliveTimeout time.Duration
 
 	sync.Mutex
-	conns map[string][]*poolConn
+	pms map[string]*poolManger
+}
+
+type poolManger struct {
+	isn   int // round robin, 解决随机分布不均匀问题
+	conns map[*poolConn]struct{}
 }
 
 type poolConn struct {
 	*grpc.ClientConn
+	ref     int // 引用计数
 	created int64
 }
 
 func newPool(size int, ttl time.Duration) *pool {
+
+	pool_init()
+
 	return &pool{
 		size:  size,
-		ttl:   int64(ttl.Seconds()),
-		conns: make(map[string][]*poolConn),
+		debug: defaultDebugMode,
+		pms:   make(map[string]*poolManger),
 	}
 }
 
 func (p *pool) getConn(addr string, opts ...grpc.DialOption) (*poolConn, error) {
 	p.Lock()
-	conns := p.conns[addr]
-	now := time.Now().Unix()
+	defer p.Unlock()
+	return p.pickRandom(addr, opts...)
+}
 
-	// while we have conns check age and then return one
-	// otherwise we'll create a new conn
-	for len(conns) > 0 {
-		conn := conns[len(conns)-1]
-		conns = conns[:len(conns)-1]
-		p.conns[addr] = conns
+func (p *pool) pickRandom(addr string, opts ...grpc.DialOption) (*poolConn, error) {
 
-		// if conn is old kill it and move on
-		if d := now - conn.created; d > p.ttl {
-			conn.ClientConn.Close()
-			continue
+	pm, ok := p.pms[addr]
+
+	if p.size != 0 && ok && pm != nil {
+		conns := pm.conns
+
+		// pick one at round robin
+		pm.isn++
+		if pm.isn %= p.size; pm.isn < len(conns) {
+			var i int
+			for conn := range conns {
+				if i == pm.isn {
+					// this one
+					if conn.isReady() {
+						conn.ref++
+						return conn, nil
+					}
+
+					// not ready, kick it out
+					p.removeConn(conns, conn)
+					_ = conn.Close()
+					break
+				}
+				i++
+			}
 		}
-
-		// we got a good conn, lets unlock and return it
-		p.Unlock()
-
-		return conn, nil
 	}
 
-	p.Unlock()
-
 	// create new conn
+	return p.newConn(addr, opts...)
+}
+
+// create new conn
+func (p *pool) newConn(addr string, opts ...grpc.DialOption) (*poolConn, error) {
+	opts = WithKeepaliveParams(opts...)
 	cc, err := grpc.Dial(addr, opts...)
 	if err != nil {
 		return nil, err
 	}
+	// just for test
+	//if pickDebugMode {
+	//	newConnNums++
+	//}
+	p.Debug("newConn")
+	return &poolConn{ClientConn: cc, ref: 1}, err
+}
 
-	return &poolConn{cc, time.Now().Unix()}, nil
+func (p *pool) removeConn(conns map[*poolConn]struct{}, conn *poolConn) {
+	if conns != nil && conn != nil {
+		delete(conns, conn)
+		return
+	}
+	p.Debug("removeConn err, conns:%v, conn:%v \n", conns, conn)
 }
 
 func (p *pool) release(addr string, conn *poolConn, err error) {
-	// don't store the conn if it has errored
-	if err != nil {
-		conn.ClientConn.Close()
+	p.Lock()
+	defer p.Unlock()
+
+	// 池子大小为0, 也就没有放入的必要了
+	if p.size == 0 {
+		_ = conn.Close()
+		return
+	}
+
+	// conn is never nil
+	conn.ref--
+
+	// pm may be nil
+	if _, ok := p.pms[addr]; !ok {
+		p.pms[addr] = &poolManger{conns: make(map[*poolConn]struct{})}
+	}
+
+	conns := p.pms[addr].conns
+
+	if err != nil || !conn.isReady() {
+		p.removeConn(conns, conn)
+		_ = conn.Close()
 		return
 	}
 
 	// otherwise put it back for reuse
-	p.Lock()
-	conns := p.conns[addr]
-	if len(conns) >= p.size {
-		p.Unlock()
-		conn.ClientConn.Close()
+	p.putConn(conns, conn)
+
+	return
+}
+
+func (p *pool) putConn(conns map[*poolConn]struct{}, conn *poolConn) {
+	if conns != nil && conn != nil {
+		if len(conns) < p.size {
+			p.Debug("put conn to pool")
+			conns[conn] = struct{}{}
+			return
+		}
+		// 不在连接池内并且连接池已经满了，那就释放掉
+		if _, ok := conns[conn]; !ok {
+			_ = conn.Close()
+		}
 		return
 	}
-	p.conns[addr] = append(conns, conn)
-	p.Unlock()
+	p.Debug("putConn err, conns:%v, conn:%v \n", conns, conn)
+}
+
+func (p *pool) Debug(a ...interface{}) {
+	if !p.debug {
+		return
+	}
+	if len(a) > 1 {
+		fmt.Printf(a[0].(string), a[1:]...)
+		return
+	}
+	fmt.Println("[debug] ", a)
+}
+
+func (cc *poolConn) isReady() bool {
+	if cc.GetState() != connectivity.Ready {
+		//if !pickDebugMode && cc.GetState() != connectivity.Ready { // just for test
+		return false
+	}
+	return true
+}
+
+func (cc *poolConn) Close() error {
+	if cc.ref == 0 {
+		return cc.ClientConn.Close()
+	}
+	return nil
 }
--- client/grpc/grpc_pool_test.go
+++ client/grpc/grpc_pool_test.go
@@ -1,64 +1,121 @@
 package grpc
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
 	"net"
+	"sync"
 	"testing"
 	"time"
 
-	"context"
 	"google.golang.org/grpc"
-	pgrpc "google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
-func testPool(t *testing.T, size int, ttl time.Duration) {
-	// setup server
-	l, err := net.Listen("tcp", ":0")
+func TestGRPCPool(t *testing.T) {
+	defaultDebugMode = true
+	// Total number of requests = conns * duration/1s * 2
+	testPoolConcurrency(t, 5, "127.0.0.1:50053", 50, 2)
+}
+
+func testPoolConcurrency(t *testing.T, poolSize int, addr string, conns int, duration int) {
+	go runServer(t)
+	rand.Seed(time.Now().UnixNano())
+
+	p := newPool(poolSize, 0)
+
+	fmt.Println("第一波 带错误")
+	invokeConcurrency(t, p, addr, conns, duration, true)
+	invokeConcurrency(t, p, addr, conns, duration, false)
+	time.Sleep(time.Second * 2)
+	fmt.Println("第二波 不带错误,不应该出现 new conn")
+	invokeConcurrency(t, p, addr, conns, duration, false)
+
+	if len(p.pms[addr].conns) != poolSize {
+		t.Fatalf("池子没有被塞满,当前池子大小: %d \n", len(p.pms[addr].conns))
+	}
+	fmt.Println("连接池已塞满")
+}
+
+func invokeConcurrency(t *testing.T, p *pool, addr string, conns int, duration int, withErr bool) {
+	wg := sync.WaitGroup{}
+	lock := &sync.Mutex{}
+	cond := sync.NewCond(lock)
+
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			lock.Lock()
+			cond.Broadcast()
+			lock.Unlock()
+		}
+	}()
+
+	for i := 0; i < conns; i++ {
+		wg.Add(duration)
+		go func() {
+			for i1 := 0; i1 < duration; i1++ {
+
+				lock.Lock()
+				cond.Wait()
+				lock.Unlock()
+
+				for i2 := 0; i2 < 2; i2++ {
+					// get a conn
+					invoke(t, p, addr, withErr)
+				}
+				wg.Done()
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func invoke(t *testing.T, p *pool, addr string, withErr bool) {
+	cc, err := p.getConn(addr, grpc.WithInsecure())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c := pb.NewGreeterClient(cc.ClientConn)
+	ctx, _ := context.WithTimeout(context.TODO(), time.Second*2)
+	rsp, err := c.SayHello(ctx, &pb.HelloRequest{Name: "John"})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if rsp.Message != "Hello John" {
+		t.Fatalf("Got unexpected response %v \n", rsp.Message)
+	}
+
+	if withErr && rand.Intn(10) == 8 {
+		fmt.Println("故意错误")
+		err = errors.New("ha ha ha")
+	}
+	// release the conn
+	p.release(addr, cc, err)
+}
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	time.Sleep(time.Millisecond * 1500)
+	return &pb.HelloReply{Message: "Hello John"}, nil
+}
+
+func runServer(t *testing.T) {
+	lis, err := net.Listen("tcp", ":50053")
 	if err != nil {
 		t.Fatalf("failed to listen: %v", err)
 	}
-	defer l.Close()
 
-	s := pgrpc.NewServer()
-	pb.RegisterGreeterServer(s, &greeterServer{})
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
 
-	go s.Serve(l)
-	defer s.Stop()
-
-	// zero pool
-	p := newPool(size, ttl)
-
-	for i := 0; i < 10; i++ {
-		// get a conn
-		cc, err := p.getConn(l.Addr().String(), grpc.WithInsecure())
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		rsp := pb.HelloReply{}
-
-		err = cc.Invoke(context.TODO(), "/helloworld.Greeter/SayHello", &pb.HelloRequest{Name: "John"}, &rsp)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if rsp.Message != "Hello John" {
-			t.Fatalf("Got unexpected response %v", rsp.Message)
-		}
-
-		// release the conn
-		p.release(l.Addr().String(), cc, nil)
-
-		p.Lock()
-		if i := len(p.conns[l.Addr().String()]); i > size {
-			p.Unlock()
-			t.Fatalf("pool size %d is greater than expected %d", i, size)
-		}
-		p.Unlock()
+	if err := s.Serve(lis); err != nil {
+		t.Fatalf("failed to serve: %v", err)
 	}
-}
-
-func TestGRPCPool(t *testing.T) {
-	testPool(t, 0, time.Minute)
-	testPool(t, 2, time.Minute)
 }
