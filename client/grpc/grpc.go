f5367806ba985827c4cc4d769c8452f8f8549b10
wuyx <wuyx@weipaitang.com>
2020-07-20 11:51:50 +0800

    定时更新不覆盖5秒内的 watch 结果。增加 grpc call 的 error 日志及当前使用节点信息

--- client/grpc/grpc.go
+++ client/grpc/grpc.go
@@ -295,7 +295,24 @@ func (g *grpcClient) NewRequest(service, method string, req interface{}, reqOpts
 	return newGRPCRequest(service, method, req, g.opts.ContentType, reqOpts...)
 }
 
-func (g *grpcClient) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+func (g *grpcClient) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) (callErr error) {
+	var callNode *registry.Node
+	defer func() {
+		if callErr != nil {
+			if callNode == nil {
+				return
+			}
+
+			if v, ok := callErr.(*errors.Error); ok {
+				v.Detail = fmt.Sprintf("error: %v, node: %v", v.Detail, callNode.Id)
+				callErr = v
+			} else {
+				callErr = fmt.Errorf("error: %v, node: %v", callErr, callNode.Id)
+			}
+			return
+		}
+	}()
+
 	// make a copy of call opts
 	callOpts := g.opts.CallOptions
 	for _, opt := range opts {
@@ -354,6 +371,9 @@ func (g *grpcClient) Call(ctx context.Context, req client.Request, rsp interface
 		} else if err != nil {
 			return errors.InternalServerError("go.micro.client", err.Error())
 		}
+
+		// save selected node
+		callNode = node
 
 		// make the call
 		err = gcall(ctx, node, req, rsp, callOpts)
